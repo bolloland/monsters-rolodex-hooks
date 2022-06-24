83eb5721408b85ed091333a922134e7f3f70756d
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-24 16:58:36 -0500

    create functional App component import search

--- src/App.js
+++ src/App.js
@@ -1,62 +1,83 @@
 import './App.css';
-import { Component } from 'react';
+
 import MonsterContainer from './components/monsterlist/MonsterContainer';
 import SearchBox from './components/searchbar/SearchBox';
 
+import React from 'react'
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
 
-//initial State
-    this.state = {
-      monsters: [], 
-      searchfield: ""
-    }
-  }
-
-  //access API (lifecycle methods):
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(resp => resp.json())
-    //pull json data from API, and that data will fill the monsters array
-    .then((users) => {
-      this.setState(
-        () => {return ({monsters: users})}
-          )})
-    }
-
-  onSearchChange = (e) => {
+  const onSearchChange = (e) => {
     const searchfield = e.target.value.toLowerCase()
       this.setState(() => {
         return {searchfield}
       })}
       
-//what to show
-  render() {
-
-    const { monsters, searchfield } = this.state
-    const { onSearchChange } = this
-
-    const filterMonsters = monsters.filter(
-    monst => {return monst.name.toLowerCase().includes(searchfield)})
-
   return (
     <div className="App">
      
       <h3 className="app-title">Monsters Rolodex (Hooks)</h3>
-        <SearchBox onSearchChange={onSearchChange} placeholder="search monsters..." className="search-box" />
-        <MonsterContainer monsters={filterMonsters} />
+      <SearchBox 
+        onSearchChange={onSearchChange} 
+        placeholder="search monsters..." 
+        className="search-box" />
 
-     {/* {filterMonsters.map((monst) => {
-          return (
-            <div key={monst.id}>
-              <h1>{monst.name} from {monst.address.city}</h1>
-            </div>)
-        })} */}
+      {/* <MonsterContainer monsters={filterMonsters} /> */}
     </div>
-  );
+  )
 }
-}
+export default App
 
-export default App;
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+// //initial State
+//     this.state = {
+//       monsters: [], 
+//       searchfield: ""
+//     }
+//   }
+
+//   //access API (lifecycle methods):
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(resp => resp.json())
+//     //pull json data from API, and that data will fill the monsters array
+//     .then((users) => {
+//       this.setState(
+//         () => {return ({monsters: users})}
+//           )})
+//     }
+
+  
+      
+// //what to show
+//   render() {
+
+//     const { monsters, searchfield } = this.state
+//     const { onSearchChange } = this
+
+//     const filterMonsters = monsters.filter(
+//     monst => {return monst.name.toLowerCase().includes(searchfield)})
+
+//   return (
+//     // <div className="App">
+     
+//     //   <h3 className="app-title">Monsters Rolodex (Hooks)</h3>
+//     //     <SearchBox onSearchChange={onSearchChange} placeholder="search monsters..." className="search-box" />
+//     //     <MonsterContainer monsters={filterMonsters} />
+
+//     //  {/* {filterMonsters.map((monst) => {
+//     //       return (
+//     //         <div key={monst.id}>
+//     //           <h1>{monst.name} from {monst.address.city}</h1>
+//     //         </div>)
+//     //     })} */}
+//     // </div>
+//   );
+// }
+// }
+
+// export default App;
