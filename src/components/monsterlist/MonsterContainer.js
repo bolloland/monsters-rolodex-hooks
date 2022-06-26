949f869162e18618291ea4a654e34c8964c11455
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-26 11:44:39 -0500

    Monst Container to functional component

--- src/components/monsterlist/MonsterContainer.js
+++ src/components/monsterlist/MonsterContainer.js
@@ -1,26 +1,19 @@
-import { Component } from "react";
 import './card-list.styles.css'
 import './card-container.styles.css'
 import MonsterCard from './MonsterCard'
 
+import React from 'react'
 
-class MonsterContainer extends Component {
+const MonsterContainer = ({monsters}) => {
 
-    render() {
-        const { monsters } = this.props
-        
-        return (
-        <div className='card-list'>
-          {monsters.map(monster => {
-            return (
-              <MonsterCard monster={monster} className="card-container" />
-            )})}
-        </div>
-        )
-    }
+  return (
+    <div className='card-list'>
+    {monsters.map(monster => {
+      return (
+        <MonsterCard monster={monster} className="card-container" />
+      )})}
+  </div>
+  )
 }
 
-
 export default MonsterContainer
-
-      
