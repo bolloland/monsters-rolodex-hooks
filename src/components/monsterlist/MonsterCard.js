99e90cb2c87b80a3be7ae8ccbdaf599d6b998957
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-26 11:44:15 -0500

    Monstercard to functional component

--- src/components/monsterlist/MonsterCard.js
+++ src/components/monsterlist/MonsterCard.js
@@ -1,17 +1,18 @@
-import { Component } from "react";
 import './card-container.styles.css'
 
-class MonsterCard extends Component {
-    render () {
-        const { id, name, email } = this.props.monster
-        return (
-            <div className="card-container" key={id}>
-            <img alt={`Monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
-            <h2>{name}</h2>
-            <p>{email}</p>
-          </div>
+import React from 'react'
 
-        )
-    }}
-    
+const MonsterCard = ({monster}) => {
+    console.log({monster})
+    const { id, name, email } = monster
+    return (
+        <div className="card-container" key={id}>
+        <img alt={`Monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+
+    )
+}
+
 export default MonsterCard
