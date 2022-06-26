9240e26f5e34f49ab55705445f412a64c39d2f00
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-26 11:44:53 -0500

    SearchBox to functional component

--- src/components/searchbar/SearchBox.js
+++ src/components/searchbar/SearchBox.js
@@ -1,26 +1,17 @@
 import React from 'react'
-import { Component } from 'react'
 import './search-box.styles.css'
 
-class SearchBox extends Component {
-    // onSearchChange = (e) => {
-    //     const searchfield = e.target.value.toLowerCase()
-    //       this.setState(() => {
-    //         return {searchfield}
-    //       })}
 
-  render() {
-        const {onSearchChange, placeholder, className} = this.props
-        return (
-            <input 
-                className={className}
-                type="search" 
-                placeholder={placeholder}
-                onChange={onSearchChange} 
-            />
-        )
-    }
+const SearchBox = (props) => {
+    const {onSearchChange, placeholder, className} = props
+    return (
+        <input 
+            className={className}
+            type="search" 
+            placeholder={placeholder}
+            onChange={onSearchChange} 
+        />
+    )
 }
-
 
 export default SearchBox
